f3064d1e7dd9b2ca3ea5561ce6bec79d08aaf46a
AntonStognij <antonstognij5@gmail.com>
2022-05-11 16:11:20 +0300

    completed homework

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 //функция проверки ввода времени
 function checkTime(time){
-    if(!time) return false; //проверяем на пустоту ':'
-    if(!time.includes(':')) return false; //проверяем наличие ':'
-    if (time.length != 5) return false; //проверяем длину текста
+    if(!time ||!time.includes(':')|| time.length != 5) return false; 
     var patStingOne = +time.slice(0, 2); //отделяем часы
     var patStingTwo = +time.slice(3, 5); //отделяем минуты
-    console.log(patStingOne, patStingTwo)
-    if (typeof patStingOne != "number" || typeof patStingTwo != "number") return false; // проверяме значения на тип
-    if (isNaN(patStingOne) || isNaN(patStingTwo)) return false; // проверяме значения на NaN
-    if (patStingOne >23 || patStingOne < 0) return false;  // проверяме часы (не больше 23 часов)
-    if (patStingTwo > 59 || patStingTwo < 0) return false; // проверяме минуты (не больше 59 часов)
+    if (typeof patStingOne != "number" || typeof patStingTwo != "number"||isNaN(patStingOne) || isNaN(patStingTwo)||patStingOne >23|| patStingOne < 0 || patStingTwo > 59 || patStingTwo < 0) return false; 
     return true
 }
 
 //запускаем цикл с вопросом 
 for (;;){
-    var timeEnd = prompt('Когда Вас разбудить? Укажите время в формате "чч:мм"');
+    var timeEnd = prompt('Когда Вас разбудить? Укажите время в формате "чч:мм".');
     if (timeEnd === null) {
         break;
     } 
@@ -26,16 +20,14 @@ for (;;){
         break;
     }
 }
-console.log(timeEnd);
 
-//проверям время
+//отображаем время и делаем сигнал будильника
 setInterval(function (){
     var date = new Date();
     var time = date.toLocaleTimeString()
     document.getElementById('block').innerHTML = time
     if (time === timeEnd) {
-        document.getElementById('audio').setAttribute('muted', false)
-        alert('Звонок');
+        alert('Звонок.');
     }
     
 }, 1000)
